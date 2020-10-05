62e582982c58a68302ec83fa7a4e22300e45e1be
Edwin Torres <codesandtags@gmail.com>
2020-10-05 10:49:53 -0500

    feature: Added twitter button event to share quotes

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -4,29 +4,56 @@ const authorText = document.querySelector("#author");
 const twitterButton = document.querySelector("#twitter");
 const newQuoteButton = document.querySelector("#new-quote");
 
+const getAuthor = (author) => {
+  return author === "" ? "- Unknown" : `- ${author}`;
+};
+
+const validateQuoteTextStyle = (quote, quoteTextSelector) => {
+  if (quote.length > 120) {
+    quoteTextSelector.classList.add("long-quote");
+  } else {
+    quoteTextSelector.classList.remove("long-quote");
+  }
+};
+
 // http://forismatic.com/en/api
 // GET Quote from API
 const getQuote = async () => {
   const proxyURL = "https://cors-anywhere.herokuapp.com/";
   const apiURL =
     "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+  const headers = {
+    origin: "x-requested-with",
+  };
+
   try {
     const response = await fetch(`${proxyURL}${apiURL}`, {
-      headers: {
-        origin: "x-requested-with",
-      },
+      headers: headers,
     });
-    const quote = await response.json();
+    const data = await response.json();
 
-    authorText.innerText = quote.quoteAuthor;
-    quoteText.innerText = quote.quoteText;
+    authorText.innerText = getAuthor(data.quoteAuthor);
+    quoteText.innerText = data.quoteText;
+    validateQuoteTextStyle(data.quoteText, quoteText);
   } catch (error) {
     debugger;
+    setTimeout(() => {
+      getQuote();
+    }, 1000);
     console.error("Upss, there is an error here ", error);
   }
 };
 
+const twiteetQuote = () => {
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+
+  window.open(twitterUrl, "_blank");
+};
+
 // Listeners
 newQuoteButton.addEventListener("click", getQuote);
+twitterButton.addEventListener("click", twiteetQuote);
 
 getQuote();
