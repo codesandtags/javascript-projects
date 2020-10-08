4d5147a62aa2f713adb7a04dd4125a8e1429ce6f
Edwin Torres <codesandtags@gmail.com>
2020-10-08 13:52:43 -0500

    docs: Added documentation and small fixes

--- index.md
+++ index.md
@@ -5,6 +5,7 @@ This project contains a bunch of projects made with JavaScript, and the most of 
 ## Projects
 
 - [Quotes Generator](https://codesandtags.github.io/javascript-projects/quote-generator/) : Use fetch to get quotes from an API, handles JavaScript events, and allow you to share the quote in twitter.
+- [Infinite Scroll](https://codesandtags.github.io/javascript-projects/infinite-scroll/) : Fetch images from Unsplash API and render the images, when the scroll downs, fetch more images.
 
 ## Contact
 
--- infinite-scroll/script.js
+++ infinite-scroll/script.js
@@ -4,7 +4,7 @@ const loader = document.querySelector("#loader");
 let photos = [];
 
 // Unsplash
-const countPhotos = 5;
+const countPhotos = 10;
 const apiKey =
   "183b5711ea11e14d521e7b9bf2572333b9c0e9de0c778b9c6f15e40cfdf42d51";
 const UNSPLASH_URL = `https://api.unsplash.com/photos/random/?count=${countPhotos}&client_id=${apiKey}&orientation=landscape`;
@@ -17,22 +17,31 @@ const hideLoader = () => {
   loader.style.display = "none";
 };
 
+const setAttributes = (element, attributes) => {
+  for (key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+};
+
 const displayPhotos = (photos) => {
   photos.forEach((photo) => {
     const item = document.createElement("a");
-    item.setAttribute("href", photo.links.html);
-    item.setAttribute("target", "_blank");
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
 
     const figure = document.createElement("figure");
     const figureCaption = document.createElement("figcaption");
     figureCaption.innerText = `Taken by ${photo.user.name}, ${photo.user.location}`;
 
     const img = document.createElement("img");
-    img.setAttribute("src", photo.urls.regular);
-    img.setAttribute("alt", photo.alt_description);
-    img.setAttribute("title", photo.alt_description);
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
 
-    debugger;
     figure.appendChild(img);
     figure.appendChild(figureCaption);
     item.appendChild(figure);
