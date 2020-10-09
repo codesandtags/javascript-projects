e8f0da87da9277b84cfc972e0e7c8f13775d5bf3
Edwin Torres <codesandtags@gmail.com>
2020-10-09 16:14:21 -0500

    feature: Added picture in picture feature for navigator

---
+++ picture-in-picture/script.js
@@ -0,0 +1,22 @@
+const videoElement = document.querySelector("#video");
+const buttonElemnt = document.querySelector("#button");
+
+const selectMediaStream = async () => {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    debugger;
+    console.log(error);
+  }
+};
+
+buttonElemnt.addEventListener("click", async () => {
+  buttonElemnt.disabled = true;
+  await selectMediaStream();
+  buttonElemnt.disabled = false;
+});
